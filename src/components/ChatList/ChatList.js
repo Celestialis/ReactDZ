c8af36b0fc12e8e0cd1a8a11c6c84be3a07bff9c
Celestialis <nagibatorkir@gmail.com>
2022-04-25 18:58:25 +0700

    9Урок

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {Link, Outlet} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link, Outlet, useParams} from "react-router-dom";
 import './ChatList.scss';
 import {Form} from "../Form/Form";
 import {selectChats} from "../../store/chats/selector";
@@ -9,22 +9,36 @@ import {initMessageForChat, removeMessage} from "../../store/messages/actions";
 import shallowEqual from "react-redux/lib/utils/shallowEqual";
 import Button from "@mui/material/Button"
 import DeleteIcon from '@mui/icons-material/Delete';
+import {set, onValue, remove} from "firebase/database";
+import {chatsRef, chatsUserUidRef, getChatRefById, getMsgsRefById} from "../../services/firebase";
+import {auth} from "../../services/firebase";
 
 export const ChatList = () => {
     const dispatch = useDispatch();
-    const chats = useSelector(selectChats, shallowEqual);
+    const [chats, setChats] = useState([]);
     const handleSubmit = (newChatName) => {
         const newChat = {
             name: newChatName,
             id: `chat-${Date.now()}`,
         };
-        dispatch(addChat(newChat));
+        set(getChatRefById(newChat.id), newChat);
+        set(getMsgsRefById(newChat.id), {exists: true});
+        set(chatsUserUidRef(newChat.id), [auth.currentUser.uid]);
         dispatch(initMessageForChat(newChat.id));
     };
     const handleRemoveChat = (id) => {
-        dispatch(deleteChat(id));
+        remove(getChatRefById(id));
+        set(getMsgsRefById(id), null);
         dispatch(removeMessage(id));
     }
+    
+    useEffect(() => {
+        const unsubscribe = onValue(chatsRef, (snapshot) => {
+
+            setChats(Object.values(snapshot.val() || {}));
+        });
+        return unsubscribe;
+    }, []);
 
     return (
         <>
